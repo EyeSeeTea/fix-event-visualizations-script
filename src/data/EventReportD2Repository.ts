882e73ff1c6080d667e24877e7dd57f3fd6bdf52
p3rcypj <pablo@percypj.com>
2023-11-14 13:36:50 +0000

    Use correct api schema

--- src/data/EventReportD2Repository.ts
+++ src/data/EventReportD2Repository.ts
@@ -2,21 +2,18 @@ import _ from "lodash";
 import { D2Api, MetadataPick } from "@eyeseetea/d2-api/2.36";
 import { Async } from "domain/entities/Async";
 import { EventReportRepository } from "domain/repositories/EventReportRepository";
-import { getFieldsAsString } from "@eyeseetea/d2-api/api/common";
 import { EventReport } from "domain/entities/EventReport";
 
 export class EventReportD2Repository implements EventReportRepository {
     constructor(private api: D2Api) {}
 
     async getCorrupted(): Async<EventReport[]> {
-        const eventReports = await this.api
-            .get<{
-                eventReports: D2EventReport[];
-            }>("/eventReports", {
+        const eventReports = await this.api.models.eventReports
+            .get({
                 paging: false,
-                fields: getFieldsAsString(eventReportFields),
+                fields: eventReportFields,
             })
-            .map(({ data }) => data.eventReports)
+            .map(({ data }) => data.objects)
             .getData();
 
         return eventReports.map(e => this.buildEventReport(e));
